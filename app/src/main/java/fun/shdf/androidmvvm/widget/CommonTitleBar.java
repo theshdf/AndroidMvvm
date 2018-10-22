4b4c909ec52be34672c34ae1d40c065928f970c9
theshdf <theshdf@gmail.com>
2018-10-22 10:30:58 +0800

    新增了状态栏修改的类和封装

---
+++ app/src/main/java/fun/shdf/androidmvvm/widget/CommonTitleBar.java
@@ -0,0 +1,10 @@
+package fun.shdf.androidmvvm.widget;
+
+/**
+ * code-time: 2018/10/19 15:22
+ * code-author: by shdf
+ * coder-wechat: zcm656025633
+ * exp:
+ **/
+public class CommonTitleBar {
+}
---
+++ app/src/main/java/fun/shdf/androidmvvm/widget/PageStateControlView.java
@@ -0,0 +1,10 @@
+package fun.shdf.androidmvvm.widget;
+
+/**
+ * code-time: 2018/10/19 15:24
+ * code-author: by shdf
+ * coder-wechat: zcm656025633
+ * exp:
+ **/
+public class PageStateControlView {
+}
